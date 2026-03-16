c45a3f80c39ddec92825299376174707ea4dc92d
flavioyuan <fyuangouvea@gmail.com>
2026-03-15 22:45:26 -0300

    First commit

---
+++ municipios_ausencias.R
@@ -0,0 +1,15 @@
+#
+# --- Ausencias por municipio
+#
+
+data_2014 <- read.csv2(
+  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2014_1.csv"
+  )
+
+data_2018 <- read.csv2(
+  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2018_1.csv"
+)
+
+data_2022 <- read.csv2(
+  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2022_1.csv"
+)
