d709ab61d1d589d39c646475d57df0c5c2e0cb79
flavioyuan <fyuangouvea@gmail.com>
2026-03-16 21:19:59 -0300

    Colocado fileEncodigng e encoding no read.csv2.

--- municipios_ausencias.R
+++ municipios_ausencias.R
@@ -1,15 +1,21 @@
 #
-# --- Ausencias por municipio
+# --- Participacao eleitores por municipio
 #
 
 data_2014 <- read.csv2(
-  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2014_1.csv"
+  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2014_1.csv",
+  encoding = "UTF-8",
+  fileEncoding = "latin1"
   )
 
 data_2018 <- read.csv2(
-  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2018_1.csv"
+  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2018_1.csv",
+  encoding = "UTF-8",
+  fileEncoding = "latin1"
 )
 
 data_2022 <- read.csv2(
-  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2022_1.csv"
+  file = "/home/flavioyuan/Documentos/R/Projetos/Eleitoral/Eleitoral/evolucao_comp_abst-municipio_2022_1.csv",
+  encoding = "ITF-8",
+  fileEncoding = "latin1"
 )
